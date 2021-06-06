4d6837f98c9e9d106e61848ca5c35a01f7b5959b
Gulkin Igor <ardar.igor@mail.ru>
2021-06-07 01:23:17 +0700

    Update GameElementGroup.cs

--- GameElementGroup.cs
+++ GameElementGroup.cs
@@ -31,63 +31,101 @@ namespace GameElements
         {
             return this.GetEnumerator();
         }
-        
-        protected override void OnPrepareGame(object sender)
+
+        protected sealed override void OnPrepareGame(object sender)
         {
             base.OnPrepareGame(sender);
             foreach (var element in this)
             {
                 element.OnPrepareGame(sender);
             }
+
+            this.OnPrepareGame(sender, this);
         }
 
-        protected override void OnReadyGame(object sender)
+        protected virtual void OnPrepareGame(object sender, GameElementGroup _)
+        {
+        }
+
+        protected sealed override void OnReadyGame(object sender)
         {
             base.OnReadyGame(sender);
             foreach (var element in this)
             {
                 element.OnReadyGame(sender);
             }
+
+            this.OnReadyGame(sender, this);
         }
 
-        protected override void OnStartGame(object sender)
+        protected virtual void OnReadyGame(object sender, GameElementGroup _)
+        {
+        }
+
+        protected sealed override void OnStartGame(object sender)
         {
             base.OnStartGame(sender);
             foreach (var element in this)
             {
                 element.OnStartGame(sender);
             }
+
+            this.OnStartGame(sender, this);
         }
 
-        protected override void OnPauseGame(object sender)
+        protected virtual void OnStartGame(object sender, GameElementGroup _)
+        {
+        }
+
+        protected sealed override void OnPauseGame(object sender)
         {
             base.OnPauseGame(sender);
             foreach (var element in this)
             {
                 element.OnPauseGame(sender);
             }
+
+            this.OnPauseGame(sender, this);
         }
 
-        protected override void OnResumeGame(object sender)
+        protected virtual void OnPauseGame(object sender, GameElementGroup _)
+        {
+        }
+
+        protected sealed override void OnResumeGame(object sender)
         {
             base.OnResumeGame(sender);
             foreach (var element in this)
             {
                 element.OnResumeGame(sender);
             }
+
+            this.OnResumeGame(sender, this);
         }
 
-        protected override void OnFinishGame(object sender)
+        protected virtual void OnResumeGame(object sender, GameElementGroup _)
+        {
+        }
+
+        protected sealed override void OnFinishGame(object sender)
         {
             base.OnFinishGame(sender);
             foreach (var element in this)
             {
                 element.OnFinishGame(sender);
             }
+
+            this.OnFinishGame(sender, this);
         }
 
-        protected override void OnDestroyGame(object sender)
+        protected virtual void OnFinishGame(object sender, GameElementGroup _)
         {
+        }
+
+        protected sealed override void OnDestroyGame(object sender)
+        {
+            this.OnDestroyGame(sender, this);
+
             foreach (var element in this)
             {
                 element.OnDestroyGame(sender);
@@ -95,5 +133,9 @@ namespace GameElements
 
             base.OnDestroyGame(sender);
         }
-    } 
+
+        protected virtual void OnDestroyGame(object sender, GameElementGroup _)
+        {
+        }
+    }
 }
